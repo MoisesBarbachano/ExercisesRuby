102d59b7739e5291dcbcdc2c64aeeef3ced1b18f
Moises <moises.barbachano.chiu@gmail.com>
2019-11-12 13:42:09 -0600

    Exercise 3 modified with spaces

--- exercise3.rb
+++ exercise3.rb
@@ -2,11 +2,11 @@ require 'date'
 
 IS_HIGHER_MONTH = 1
 
-def month_by_month(init_date,end_date,day)
+def month_by_month(init_date, end_date, day)
   dates = []
   actual_date = Date.new(init_date.year, init_date.mon, day)
 
-  while((actual_date <=> end_date)!= IS_HIGHER_MONTH)
+  while((actual_date <=> end_date) != IS_HIGHER_MONTH)
     dates << actual_date.strftime("%e/%B/%Y")
     actual_date = actual_date.next_month
   end
@@ -14,5 +14,4 @@ def month_by_month(init_date,end_date,day)
   dates
 end
 
-
-month_by_month(Date.new(2018,1,2),Date.new(2018,5,5),4) 
+month_by_month(Date.new(2018,1,2), Date.new(2018,5,5), 4) 
