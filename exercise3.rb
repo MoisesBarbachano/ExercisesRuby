fba07cddba685644450b4e37add597cd3407ba64
Moises <moises.barbachano.chiu@gmail.com>
2019-11-12 13:45:11 -0600

    Exercise 3 changed actual to current

--- exercise3.rb
+++ exercise3.rb
@@ -4,11 +4,11 @@ IS_HIGHER_MONTH = 1
 
 def month_by_month(init_date, end_date, day)
   dates = []
-  actual_date = Date.new(init_date.year, init_date.mon, day)
+  current_date = Date.new(init_date.year, init_date.mon, day)
 
-  while((actual_date <=> end_date) != IS_HIGHER_MONTH)
-    dates << actual_date.strftime("%e/%B/%Y")
-    actual_date = actual_date.next_month
+  while((current_date <=> end_date) != IS_HIGHER_MONTH)
+    dates << current_date.strftime("%e/%B/%Y")
+    current_date = current_date.next_month
   end
 
   dates
