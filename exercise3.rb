83916b9978fdb37ffddaab0ac9308cab3466366a
Moises <moises.barbachano.chiu@gmail.com>
2019-11-12 13:48:30 -0600

    Exercise 3 modified IS_HIGHER_MONTH to HIGHER_MONTH

--- exercise3.rb
+++ exercise3.rb
@@ -1,12 +1,12 @@
 require 'date'
 
-IS_HIGHER_MONTH = 1
+HIGHER_MONTH = 1
 
 def month_by_month(init_date, end_date, day)
   dates = []
   current_date = Date.new(init_date.year, init_date.mon, day)
 
-  while((current_date <=> end_date) != IS_HIGHER_MONTH)
+  while((current_date <=> end_date) != HIGHER_MONTH)
     dates << current_date.strftime("%e/%B/%Y")
     current_date = current_date.next_month
   end
