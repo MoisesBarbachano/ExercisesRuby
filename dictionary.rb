7ad528c4d2d59c95fbfc8a63eba03f00e223b1ca
Moises <moises.barbachano.chiu@gmail.com>
2019-11-20 11:32:18 -0600

    Dictionary exercise uploaded

---
+++ dictionary.rb
@@ -0,0 +1,21 @@
+class Dictionary
+  @words = { department: 'departamento',
+             tenant: 'inquilino',
+             owner: 'propietario',
+             rent: 'renta'
+  }
+
+  def self.translate(idiom, word)
+    @words.each do |english_word, spanish_word|
+      case idiom
+        when "English"
+          break english_word if spanish_word.to_s == word.to_s
+        when "Spanish"
+          break spanish_word if english_word.to_s == word.to_s
+        end
+    end
+  end
+end
+
+Dictionary.translate("English", "inquilino")
+Dictionary.translate("Spanish", "tenant")
