875a26d21e99fa37d46cb54594890ab1a4db4f36
Moises <moises.barbachano.chiu@gmail.com>
2019-11-20 10:06:44 -0600

    The car is ready

---
+++ car.rb
@@ -0,0 +1,46 @@
+class Car
+  def initialize(brand, color, year)
+    @started = false
+    @trip_state = :not_started
+    @brand = brand
+    @color = color
+    @year = year
+  end
+
+  def start
+    begin
+      raise 'Car already started' if @started
+
+      @started = true
+    end
+
+    'Car started'
+  end
+
+  def start_trip
+    begin
+      raise 'Car is not started' unless @started
+      raise 'Trip is already started' if @trip_state == :started
+
+      @trip_state = :started
+    end
+
+    'The trip has begun'
+  end
+
+  def finish_trip
+    begin
+      raise 'Car is not started' unless @started
+      raise 'Trip is not started' if @trip_state == :not_started
+
+      @trip_state = :not_started
+    end
+
+    'The trip has finished'
+  end
+end
+
+car = Car.new('Ford', 'red', 2019)
+puts car.start
+puts car.start_trip
+puts car.finish_trip
