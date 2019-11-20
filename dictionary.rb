967d5d4578ae93fcff58d57efd63598891674a7a
Moises <moises.barbachano.chiu@gmail.com>
2019-11-20 11:49:08 -0600

    Exercise dictionary downcase

--- dictionary.rb
+++ dictionary.rb
@@ -1,21 +1,24 @@
 class Dictionary
-  @words = { department: 'departamento',
-             tenant: 'inquilino',
-             owner: 'propietario',
-             rent: 'renta'
-            }
+  @words = {
+    department: 'departamento',
+    tenant: 'inquilino',
+    owner: 'propietario',
+    rent: 'renta'
+  }
 
   def self.translate(idiom, word)
+    word = word.downcase
+    idiom = idiom.downcase
     @words.each do |english_word, spanish_word|
       case idiom
-        when "English"
+        when 'english'
           break english_word if spanish_word.to_s == word.to_s
-        when "Spanish"
+        when 'spanish'
           break spanish_word if english_word.to_s == word.to_s
         end
     end
   end
 end
 
-Dictionary.translate("English", "inquilino")
-Dictionary.translate("Spanish", "tenant")
+puts Dictionary.translate('english', 'Inquilino')
+puts Dictionary.translate('Spanish', 'Tenant')
