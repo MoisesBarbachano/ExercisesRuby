f96c8ead633dfc3e80001c667474ed88a8fd67fd
Moises <moises.barbachano.chiu@gmail.com>
2019-12-13 15:46:16 -0600

    work place added to mongodb app

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,9 +1,9 @@
 class User
   include Mongoid::Document
-  validates :email, uniqueness: true, acceptance: { message: 'Ya repetiste este email' }
   validates :mobile_phone, numericality: true
   field :email, type: String
   field :name, type: String
   field :last_name, type: String
   field :mobile_phone, type: String
+  field :work_place, type: String
 end
