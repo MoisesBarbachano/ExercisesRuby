1e7a40f15d0f9cd989db651d7361d05147fce756
Moises <moises.barbachano.chiu@gmail.com>
2020-01-24 12:39:53 -0600

    check if words are in the phrase with less code

--- find_words.rb
+++ find_words.rb
@@ -1,24 +1,12 @@
 def phrase_include?(words, phrase)
-  index_words = []
-  found_words = []
+  found_words = {}
 
-  found_words = words.keep_if do |word|
-    index_words << phrase.index(word) if phrase.include?(word)
+  words.each do |word|
+    found_words[phrase.index(word)] = word if phrase.include?(word)
   end
+  found_words = found_words.sort.to_h
 
-  order_words(found_words, index_words)
-end
-
-def order_words(words, index_words)
-  ordered_words = {}
-
-  words.each_with_index do |word, index|
-    word_order = index_words[index]
-    ordered_words[word_order] = word
-  end
-  ordered_words = ordered_words.sort.to_h
-
-  ordered_words.values
+  found_words.values
 end
 
 phrase_include?(['hola', 'de', 'nuevo'], 'denuevohola')
